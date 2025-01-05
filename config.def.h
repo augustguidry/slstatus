c7af9ae92fb720ef042d06decc434a1d07aa025f
August Guidry <august@augustguidry.com>
2025-01-05 16:30:40 -0600

    modified config

--- config.def.h
+++ config.def.h
@@ -64,6 +64,9 @@ static const char unknown_str[] = "n/a";
  * wifi_perc           WiFi signal in percent          interface name (wlan0)
  */
 static const struct arg args[] = {
-	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	/* function          format                          argument */
+  { wifi_perc,         "[WiFi %s%] ",                  "wlan0" },
+  { battery_perc,      "[BAT1 %s%] ",                   "BAT1" },
+  { battery_perc,      "[BAT0 %s%] ",                   "BAT0" },
+	{ datetime,           "%s",                          "%F %T" },
 };
